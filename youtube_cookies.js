24fd3b8fe530e9d985efac7e19b938c67c02cf5b
MovemDimon <movem9013@gmail.com>
2025-08-13 11:51:06 +0330

    Create youtube_cookies.js

---
+++ youtube_cookies.js
@@ -0,0 +1,30 @@
+export const ACCOUNTS = [
+  {
+    name: "user1",
+    cookie: process.env.COOKIE1,
+  },
+  {
+    name: "user2",
+    cookie: process.env.COOKIE2,
+  },
+  {
+    name: "user3",
+    cookie: process.env.COOKIE3,
+  },
+  {
+    name: "user4",
+    cookie: process.env.COOKIE4,
+  },
+  {
+    name: "user5",
+    cookie: process.env.COOKIE5,
+  },
+  {
+    name: "user6",
+    cookie: process.env.COOKIE6,
+  },
+  {
+    name: "user7",
+    cookie: process.env.COOKIE7,
+  }
+];
